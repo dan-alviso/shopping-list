5397e43d2b3b98ef0579ea10327935aa79b1e8ee
Daniel Alviso <dan.alviso@yahoo.com>
2018-08-25 21:28:34 -0700

    Added items route and Item model

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const items = require("./routes/api/items");
+
 const app = express();
 
 // Body parser middleware
@@ -14,6 +16,9 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
+
+// Use Routes
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 
