007bb7a7687143838bc6b3a1a6b3de050cc98aa8
Daniel Alviso <dan.alviso@yahoo.com>
2018-08-30 12:05:45 -0700

    Prepared for deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, {useNewUrlParser: true}) // Adding new mongo url parser
+  .connect(db)
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
